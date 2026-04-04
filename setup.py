d1a54092259f36ee4ed01e98b07f7fc40751a950
Samuel Oladji <samueloladji@Samuels-MacBook-Air.local>
2026-04-04 01:55:46 -0700

    Rebrand to Vaultak

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="agentbreaker",
+    name="vaultak",
     version="0.1.0",
+    author="Samuel Oladji",
+    author_email="samueloladji@gmail.com",
     description="Real-time behavioral kill switch for AI agents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samueloladji-beep/Agentbreaker-",
     packages=find_packages(),
     python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
